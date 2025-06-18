50228535684c00f6168567b24259f3fdfeb88e16
harris-giki <harris-giki@gmail.com>
2025-06-18 16:15:35 +0500

    sending responses

--- CodeConcepts/server.js
+++ CodeConcepts/server.js
@@ -3,7 +3,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("req made"); // this code will be running at the server
+  // the following code will be running at the server
+  console.log("req made");
+  // or you can access more details like
+  console.log(req.url, req.method);
+
+  // writing response set header content type
+  res.setHeader("Content-Type", "text/html");
+  res.write("<p>hello world</p>");
+  res.end(); //
 });
 
 // the server is listening for the requests using the local host at port 3000
