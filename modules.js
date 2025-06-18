8a3207849f84f4a553b9bb7b5bbf9e27a218c4ce
harris-giki <harris-giki@gmail.com>
2025-06-18 13:55:38 +0500

    Leearning Modules and Require

--- modules.js
+++ modules.js
@@ -3,3 +3,14 @@
 const xyz = require("./people");
 
 console.log(xyz); //this returns an empty object
+
+//now if we want only one property from the object containing multiple being exported
+
+const { ages } = require("./people");
+
+console.log(ages);
+
+// lets trying using these techniques to find information about the os
+
+const os = require("os");
+console.log(os.platform(), os.homedir);
--- people.js
+++ people.js
@@ -1,4 +1,16 @@
 // modular code demonstration
 
 const people = ["haris", "bilal", "ali"];
+const age = ["20", "6", "1"];
 console.log(people);
+
+// but what if we explicity want to export a certain part of the code
+
+module.exports = people; // only way to export a variable to another file
+
+//now exporting multiple properties using objects
+
+module.exports;
+{
+  people, ages;
+}
