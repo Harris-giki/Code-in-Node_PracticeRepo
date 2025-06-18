cc6c1dd517e744685638cb1844a50f7bfd366f70
harris-giki <harris-giki@gmail.com>
2025-06-18 18:46:05 +0500

    Sending HTML files

--- Express/app.js
+++ Express/app.js
@@ -7,6 +7,11 @@ const app = express();
 // listen for requests
 app.listen(3000);
 
+// routing and sending html files
 app.get("/", (req, res) => {
-  res.send("<p> home page</p>");
+  //res.send("<p> home page</p>");
+  res.sendfile("./CodeConcepts/views/index.html", { root: __dirname });
+});
+app.get("/about", (req, res) => {
+  res.send("<p> about</p>");
 });
